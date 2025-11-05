b6c14304d5546164ce1da91702ce9fd1312904f6
Tomas Araujo <tomas.araujo@correo.unimet.edu.ve>
2025-11-05 19:41:25 -0400

    commit inicial

---
+++ Project2OS/src/main/java/com/mycompany/project2os/Project2OS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.project2os;
+
+/**
+ *
+ * @author tomasaraujo
+ */
+public class Project2OS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
